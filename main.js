93fabca7b6a9bee8862389d72233043a23131480
Lydia Maude <lydia.maude@gmail.com>
2019-06-24 20:28:24 +0100

    help

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
+<script type="text/javascript">
+function randomlinks(){
+    var myrandom=Math.round(Math.random()*4)
+    var links=new Array()
+    links[0]="https://www.delish.com/cooking/recipe-ideas/a19665622/easy-chicken-fajitas-recipe/";
+    links[1]="https://www.delish.com/cooking/recipe-ideas/a26091626/easy-calzone-recipe/";
+    links[2]="https://www.delish.com/cooking/recipe-ideas/recipes/a55661/easy-crockpot-chicken-noodle-soup-recipe/";
+    links[3]="https://www.delish.com/cooking/recipe-ideas/recipes/a49800/sloppy-joe-grilled-cheese-recipe/";
+    links[4]="https://www.delish.com/cooking/recipe-ideas/recipes/a55501/best-goulash-recipe/";
 
+    window.location=links[myrandom]
+}
+</script>
 <form>
-<input type="button" value="random link!" onClick="random_3()">
+<input type="button" value="FOOD" onClick="randomlinks()">
 </form>
 
+<script type="text/javascript">
+function randomlinks(){
+    var myrandom=Math.round(Math.random()*4)
+    var links=new Array()
+    links[0]="https://open.spotify.com/playlist/37i9dQZF1DX5Ozry5U6G0d";
+    links[1]="https://open.spotify.com/playlist/37i9dQZF1DWZg863fGtALu";
+    links[2]="https://open.spotify.com/playlist/4UiM5IjpEO4sOnxD9hork2";
+    links[3]="https://open.spotify.com/playlist/37i9dQZF1DX7rOY2tZUw1k";
+    links[4]="https://open.spotify.com/playlist/37i9dQZF1DWWCAjkSJsi1g?si=o43WtheXRrymUOiwnUZY3w";
 
-<script>
-    var sites = [
-        'http://www.google.com',
-        'http://www.stackoverflow.com',
-        'http://www.example.com',
-        'http://www.youtube.com'
-    ];
-
-    function randomSite() {
-        var i = parseInt(Math.random() * sites.length);
-        location.href = sites[i];
-    }
+    window.location=links[myrandom]
+}
 </script>
-<a href="#" onclick="randomSite();">Random</a>
+<form>
+<input type="button" value="MUSIC" onClick="randomlinks()">
+</form>
