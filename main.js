8e30a8c2bb686ce912806629b640dc4fef3ca232
Lydia Maude <lydia.maude@gmail.com>
2019-06-24 18:42:40 +0100

    tryna make js work

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-<script type="text/javascript'>
-function random_3(){
-    var myrandom=Math.round(Math.random()*2)
-    var link1="http://www.codingforums.com"
-    var link2="http://www.cssdrive.com"
-    var link3="http://www.dynamicdrive.com"
-    if (myrandom==0)
-        window.location=link1
-    else if (myrandom==1)
-        window.location=link2
-    else if (myrandom==2)
-        window.location=link3
-}
-</script>
+
 <form>
 <input type="button" value="random link!" onClick="random_3()">
 </form>
+
+
+<script>
+    var sites = [
+        'http://www.google.com',
+        'http://www.stackoverflow.com',
+        'http://www.example.com',
+        'http://www.youtube.com'
+    ];
+
+    function randomSite() {
+        var i = parseInt(Math.random() * sites.length);
+        location.href = sites[i];
+    }
+</script>
+<a href="#" onclick="randomSite();">Random</a>
